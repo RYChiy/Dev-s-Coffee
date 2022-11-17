01ce5c14971f78b24ac3017df6fb705f42e1ee88
Rachid Yagubi <85196512+RYChiy@users.noreply.github.com>
2022-11-17 14:26:25 +0100

    geolocation added

--- js/app.js
+++ js/app.js
@@ -129,3 +129,35 @@ if ('geolocation' in navigator) {
 } else {
   target.innerText = 'Geolocation API not supported.';
 }
+if ("DeviceOrientationEvent" in window) {
+  window.addEventListener("deviceorientation", deviceOrientationHandler, false);
+} else {
+  document.getElementById("logoContainer").innerText =
+    "Device Orientation API not supported.";
+}
+
+function deviceOrientationHandler(eventData) {
+  var tiltLR = eventData.gamma;
+  var tiltFB = eventData.beta;
+  var dir = eventData.alpha;
+
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+  var logo = document.getElementById("imgLogo");
+  logo.style.webkitTransform =
+    "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + tiltFB * -1 + "deg)";
+  logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+  logo.style.transform =
+    "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + tiltFB * -1 + "deg)";
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}

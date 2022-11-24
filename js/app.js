20a14ee8d601272be4d2f9f39b88b3b34e0a2a32
Rachid Yagubi <85196512+RYChiy@users.noreply.github.com>
2022-11-24 16:33:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -341,6 +341,27 @@ function requestPersistence() {
       });
   }
 }
+
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    displayContents(contents);
+  };
+  reader.readAsText(file);
+}
+
+function displayContents(contents) {
+  var element = document.getElementById('file-content');
+  element.textContent = contents;
+}
+
+document.getElementById('file-input')
+  .addEventListener('change', readSingleFile, false);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker

731dee93100b1117c5e43d5fcb0d49b6e0c0efa8
Rachid Yagubi <85196512+RYChiy@users.noreply.github.com>
2022-12-08 16:16:56 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,9 +51,7 @@ const showCoffees = () => {
   );
   container.innerHTML = output;
 };
-
 document.addEventListener("DOMContentLoaded", showCoffees);
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
@@ -62,7 +60,6 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
-
 function getUserMedia(constraints) {
   // if Promise-based API is available, use it
   if (navigator.mediaDevices) {
@@ -80,14 +77,12 @@ function getUserMedia(constraints) {
     });
   }
 }
-
 function getStream (type) {
   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
     !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
     alert('User Media API not supported.');
     return;
   }
-
   var constraints = {};
   constraints[type] = true;
   
@@ -111,14 +106,12 @@ function getStream (type) {
 }
 var target = document.getElementById('target');
 var watchId;
-
 function appendLocation(location, verb) {
   verb = verb || 'updated';
   var newLocation = document.createElement('p');
   newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
   target.appendChild(newLocation);
 }
-
 if ('geolocation' in navigator) {
   document.getElementById('askButton').addEventListener('click', function () {
     navigator.geolocation.getCurrentPosition(function (location) {
@@ -135,16 +128,13 @@ if ("DeviceOrientationEvent" in window) {
   document.getElementById("logoContainer").innerText =
     "Device Orientation API not supported.";
 }
-
 function deviceOrientationHandler(eventData) {
   var tiltLR = eventData.gamma;
   var tiltFB = eventData.beta;
   var dir = eventData.alpha;
-
   document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
   document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
   document.getElementById("doDirection").innerHTML = Math.round(dir);
-
   var logo = document.getElementById("imgLogo");
   logo.style.webkitTransform =
     "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + tiltFB * -1 + "deg)";
@@ -154,40 +144,32 @@ function deviceOrientationHandler(eventData) {
 }
 if ("localStorage" in window || "sessionStorage" in window) {
   var selectedEngine;
-
   var logTarget = document.getElementById("target");
   var valueInput = document.getElementById("value");
-
   var reloadInputValue = function () {
     console.log(selectedEngine, window[selectedEngine].getItem("myKey"));
     valueInput.value = window[selectedEngine].getItem("myKey") || "";
   };
-
   var selectEngine = function (engine) {
     selectedEngine = engine;
     reloadInputValue();
   };
-
   function handleChange(change) {
     var timeBadge = new Date().toTimeString().split(" ")[0];
     var newState = document.createElement("p");
     newState.innerHTML = "" + timeBadge + " " + change + ".";
     logTarget.appendChild(newState);
   }
-
   var radios = document.querySelectorAll("#selectEngine input");
   for (var i = 0; i < radios.length; ++i) {
     radios[i].addEventListener("change", function () {
       selectEngine(this.value);
     });
   }
-
   selectEngine("localStorage");
-
   valueInput.addEventListener("keyup", function () {
     window[selectedEngine].setItem("myKey", this.value);
   });
-
   var onStorageChanged = function (change) {
     var engine =
       change.storageArea === window.localStorage
@@ -208,13 +190,10 @@ if ("localStorage" in window || "sessionStorage" in window) {
       reloadInputValue();
     }
   };
-
   window.addEventListener("storage", onStorageChanged);
 }
-
 function readContacts() {
   var api = navigator.contacts || navigator.mozContacts;
-
   if (api && !!api.select) {
     // new Chrome API
     api
@@ -240,7 +219,6 @@ function readContacts() {
       sortBy: "familyName",
       sortOrder: "ascending",
     };
-
     api
       .find(criteria)
       .then(function (contacts) {
@@ -261,31 +239,24 @@ function readContacts() {
     consoleLog("Contacts API not supported.");
   }
 }
-
 function consoleLog(data) {
   var logElement = document.getElementById("log");
   logElement.innerHTML += data + "\n";
 }
-
 function getReadFile(reader, i) {
   return function () {
     var li = document.querySelector('[data-idx="' + i + '"]');
-
     li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
   }
 }
-
 function readFiles(files) {
   document.getElementById('count').innerHTML = files.length;
-
   var target = document.getElementById('target');
   target.innerHTML = '';
-
   for (var i = 0; i < files.length; ++i) {
     var item = document.createElement('li');
     item.setAttribute('data-idx', i);
     var file = files[i];
-
     var reader = new FileReader();
     reader.addEventListener('load', getReadFile(reader, i));
     reader.readAsText(file);
@@ -295,7 +266,6 @@ function readFiles(files) {
     target.appendChild(item);
   };
 }
-
 async function writeFile() {
   if (!window.chooseFileSystemEntries) {
     alert('Native File System API not supported');
@@ -316,8 +286,6 @@ async function writeFile() {
   
   target.innerHTML = 'Test content written to ' + file.name + '.';
 }
-
-
 if ('storage' in navigator && 'estimate' in navigator.storage) {
   navigator.storage.estimate()
     .then(estimate => {
@@ -326,14 +294,12 @@ if ('storage' in navigator && 'estimate' in navigator.storage) {
       document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
     });
 }
-
 if ('storage' in navigator && 'persisted' in navigator.storage) {
   navigator.storage.persisted()
     .then(persisted => {
       document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
     });
 }
-
 function requestPersistence() {
   if ('storage' in navigator && 'persist' in navigator.storage) {
     navigator.storage.persist()
@@ -342,7 +308,6 @@ function requestPersistence() {
       });
   }
 }
-
 function readSingleFile(e) {
   var file = e.target.files[0];
   if (!file) {
@@ -355,126 +320,12 @@ function readSingleFile(e) {
   };
   reader.readAsText(file);
 }
-
 function displayContents(contents) {
   var element = document.getElementById('file-content');
   element.textContent = contents;
 }
-
 document.getElementById('file-input')
   .addEventListener('change', readSingleFile, false);
-
-var $status = document.getElementById('status');
-
-if ('Notification' in window) {
-  $status.innerText = Notification.permission;
-}
-
-function requestPermission() {
-  if (!('Notification' in window)) {
-    alert('Notification API not supported!');
-    return;
-  }
-  
-  Notification.requestPermission(function (result) {
-    $status.innerText = result;
-  });
-}
-
-function nonPersistentNotification() {
-  if (!('Notification' in window)) {
-    alert('Notification API not supported!');
-    return;
-  }
-  
-  try {
-    var notification = new Notification("Hi there - non-persistent!");
-  } catch (err) {
-    alert('Notification API error: ' + err);
-  }
-}
-
-function persistentNotification() {
-  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
-    alert('Persistent Notification API not supported!');
-    return;
-  }
-  
-  try {
-    navigator.serviceWorker.getRegistration()
-      .then((reg) => reg.showNotification("Hi there - persistent!"))
-      .catch((err) => alert('Service Worker registration error: ' + err));
-  } catch (err) {
-    alert('Notification API error: ' + err);
-  }
-
-  if ('permissions' in navigator) {
-  var logTarget = document.getElementById('logTarget');
-
-  function handleChange(permissionName, newState) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newStateInfo = document.createElement('p');
-    newStateInfo.innerHTML = '' + timeBadge + ' State of ' + permissionName + ' permission status changed to ' + newState + '.';
-    logTarget.appendChild(newStateInfo);
-  }
-
-  function checkPermission(permissionName, descriptor) {
-    try {
-    navigator.permissions.query(Object.assign({name: permissionName}, descriptor))
-      .then(function (permission) {
-        document.getElementById(permissionName + '-status').innerHTML = permission.state;
-        permission.addEventListener('change', function (e) {
-          document.getElementById(permissionName + '-status').innerHTML = permission.state;
-          handleChange(permissionName, permission.state);
-        });
-      });
-    } catch (e) {
-    }
-  }
-
-  checkPermission('geolocation');
-  checkPermission('notifications');
-  checkPermission('push', {userVisibleOnly: true});
-  checkPermission('midi', {sysex: true});
-  checkPermission('camera');
-  checkPermission('microphone');
-  checkPermission('background-sync');
-  checkPermission('ambient-light-sensor');
-  checkPermission('accelerometer');
-  checkPermission('gyroscope');
-  checkPermission('magnetometer');
-
-  var noop = function () {};
-  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  
-  function requestGeolocation() {
-    navigator.geolocation.getCurrentPosition(noop);
-  }
-
-  function requestNotifications() {
-    Notification.requestPermission();
-  }
-
-  function requestPush() {
-    navigator.serviceWorker.getRegistration()
-      .then(function (serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.subscribe();
-      });
-  }
-
-  function requestMidi() {
-    navigator.requestMIDIAccess({sysex: true});
-  }
-  
-  function requestCamera() {
-    navigator.getUserMedia({video: true}, noop, noop)
-  }
-  
-  function requestMicrophone() {
-    navigator.getUserMedia({audio: true}, noop, noop)
-  }
-}
-  
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
@@ -483,5 +334,3 @@ if ("serviceWorker" in navigator) {
       .catch((err) => console.log("service worker not registered", err));
   });
 }
-
-  
